a947b20c0dac80c29e9eecca1b5f5e7e63d9ed62
Ruan.Dev <ruanlopes21@gmail.com>
2025-10-27 19:30:11 -0300

    chore: reseta histórico e inicia estrutura atual

---
+++ src/Livro.java
@@ -0,0 +1,10 @@
+import java.time.LocalDate;
+
+public class Livro {
+    Integer id;
+    String titulo;
+    String autor;
+    boolean disponivel;
+    LocalDate dataCadastro;
+    LocalDate dataAtualizacao;
+}
